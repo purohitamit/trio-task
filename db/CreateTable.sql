a1e18095231f4b4e5495d53fe6ea08a472149c90
purohitamit <Amit@vasave.uk>
2021-11-22 17:43:47 +0000

    pushing to github

--- db/CreateTable.sql
+++ db/CreateTable.sql
@@ -11,6 +11,6 @@ CREATE TABLE IF NOT EXISTS users
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Ben','Hesketh','test@test7.com'),(2,'Luke','Benson','test@test.com'),(3,'Matt','Hunt','test4@test.com');
+INSERT INTO `users` VALUES (1,'Amit','Purohit','test@test7.com'),(2,'Abdul','Butt','test@test.com'),(3,'Arvydas','kelmanas','test4@test.com'),(4,'Chris John','Reeves','test8@test.com'),(5,'Eleana Celeste','Rios Jiminez','test9@test.com'),(6,'Kaho','Choi','test10@test.com'),(7,'Krzysztof','kowalski','test11@test.com'),(8,'Kwame','Acheampong','test12@test.com'),(9,'Kyronne ','Parkes','test13@test.com'),(10,'Mark','Dance','test14@test.com'),(11,'Matthew','Farrow','test15@test.com'),(12,'Nicholas','Ghartey','test16@test.com'),(13,'Olu','Olupemi','test17@test.com'),(14,'Purvi','Thakkar','test18@test.com'),(15,'Roberto','Mapaye','test19@test.com'),(16,'Susan','Adams','test20@test.com'),(17,'Yoan Anthony','Caboste','test21@test.com');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
